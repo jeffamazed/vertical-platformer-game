a553f79573999d9115316009068833856ec7a0fd
Jeffry <jeffry1408.job@gmail.com>
2025-04-19 22:08:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -216,16 +216,17 @@ window.addEventListener("keyup", (e) => {
 // Key HTML
 
 startBtn.addEventListener("click", () => {
-	
+	startPage.classList.add("hidden");
 	gamePage.classList.remove("hidden");
 });
 
 backBtn.addEventListener("click", () => {
-	
+	startPage.classList.remove("hidden");
 	gamePage.classList.add("hidden");
 
 	window.location.reload();
 });
+
 
 
 
